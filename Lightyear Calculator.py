c034139de723f363e4b7e9c750a8ee623f14cd3f
vampirot800 <111137463+vampirot800@users.noreply.github.com>
2022-09-24 10:35:42 -0500

    Add files via upload

--- Lightyear Calculator.py
+++ Lightyear Calculator.py
@@ -1,11 +1,12 @@
 import math
 math.pow
 math.sqrt
-
-print("Bienvenido a LightYear Calculator.py!")
+ 
+print("\n \n Bienvenido a LightYear Calculator.py! \n \n")
 
 k = int(1)
 r = 0
+l = 0
 
 def calc_periodo(k,r):
     res=math.sqrt((k)*(pow(r,3)))
@@ -15,42 +16,55 @@ def calcp_meses(k,r):
     meses = (calc_periodo(k,r))*(12)
     return meses
 
-def imprime_fin(p):
-    print("El periodo orbital de",p,"en dar la vuelta al sol es de:")
+def imprime_fin1(p):
+    print("El periodo orbital de",p,"en completar una vuelta al sol es de:")
 
-MenuPrincipal = int(input("Menu Principal: \n ¿Que desea Calcular? \n 1: Periodo orbital \n 2: Periodo de rotacion \n 3: Gravedad \n 4: Lunas \n 0: Salir \n"))
+def imprime_fin4(l):
+    print("\n",p,"cuenta con un numero de:",l,"lunas confirmadas")
+
+MenuPrincipal = int(input("Menu Principal: \n \n ¿Que desea Calcular? \n \n 1: Periodo orbital \n 2: Periodo de rotacion \n 3: Gravedad \n 4: Lunas \n 0: Salir \n"))
 
 while MenuPrincipal !=0:
 
     p = input("Escriba el planeta del sistema solar de su preferencia:")
     if p.lower() == "mercurio":
         r = 0.39
+        l = 0
     if p.lower()== "venus":
         r = 0.72
+        l = 0
     if p.lower() == "tierra":
         r = 1.00
+        l = 1
     if p.lower() == "marte":
         r = 1.52
+        l = 2
     if p.lower() == "jupiter":
         r = 5.20
+        l = 79
     if p.lower() == "saturno":
         r = 9.54
+        l = 82
     if p.lower() == "urano":
         r = 19.19
+        l = 27
     if p.lower() == "neptuno":
         r = 30.06
+        l = 14
 
     if MenuPrincipal == 1: 
         format = int(input("Como desea obtener el resultado?: \n 1: Años \n 2: Meses \n"))
         if format == 1:
-            print(imprime_fin(p)),print(calc_periodo(k,r),"años")
+            (imprime_fin1(p)),print(calc_periodo(k,r),"años")
         elif format == 2:
-            print(imprime_fin(p)),print(calcp_meses(k,r),"meses")
-        
-        
+            (imprime_fin1(p)),print(calcp_meses(k,r),"meses")
+
+    elif MenuPrincipal == 4:
+        (imprime_fin4(l))
+
     else: print("Por favor ingrese una opcion valida")
 
-    MenuPrincipal = int(input("\n Menu Principal: \n ¿Que desea Calcular? \n 1: Periodo orbital \n 2: Periodo de rotacion \n 3: Gravedad \n 4: Lunas \n 0: Salir \n"))
+    MenuPrincipal = int(input("\n Menu Principal: \n \n ¿Que desea Calcular? \n \n 1: Periodo orbital \n 2: Periodo de rotacion \n 3: Gravedad \n 4: Lunas \n 0: Salir \n"))
         
 
 
