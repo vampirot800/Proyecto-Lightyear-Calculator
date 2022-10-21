f4c31c9af59406da8548b21c8062108b9b5528e9
vampirot800 <111137463+vampirot800@users.noreply.github.com>
2022-10-20 22:09:34 -0500

    Update Lightyear Calculator.py

--- Lightyear Calculator.py
+++ Lightyear Calculator.py
@@ -2,11 +2,13 @@
 """
 Proyecto Lightyear-Calculator Python.
 
-!!!ESTE PROGRAMA REQUIERE DE LA DESCARGA DE LA BIBLIOTECA TERMCOLOR!!!
+!!!ESTE PROGRAMA REQUIERE DE LA DESCARGA DE LA BIBLIOTECA TERMCOLOR,
+Y PRETTYTABLE!!!
+
 Por lo que inclui dos archivos en el repositorio:
 
 1: Lightyear Calculator.py <--- (archivo sin requerimientos de descarga)
-2: Lightyear Calculator Color.py <--- (archivo con biblioteca termcolor)
+2: LightCalc(+bibliotecas).py <--- (archivo completo con bibliotecas)
 
 
 El programa funciona como una calculadora con interfaz de menu
